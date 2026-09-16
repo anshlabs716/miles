5f3f5058700f04b8f21034be53b2750dea805adc
Ansh bhatia <bhatiaansh716@gmail.com>
2026-09-16 19:37:37 +0800

    Add one-shot activity query for real exports

--- app/src/main/java/com/example/miles/data/local/ActivityDao.kt
+++ app/src/main/java/com/example/miles/data/local/ActivityDao.kt
@@ -13,6 +13,9 @@ interface ActivityDao {
     @Query("SELECT * FROM activities WHERE isDeleted = 0 ORDER BY startTime DESC")
     fun getAllActivities(): Flow<List<ActivityEntity>>
 
+    @Query("SELECT * FROM activities WHERE isDeleted = 0 ORDER BY startTime DESC")
+    suspend fun getAllActivitiesOnce(): List<ActivityEntity>
+
     @Query("SELECT * FROM activities WHERE isDeleted = 0 AND isFavorite = 1 ORDER BY startTime DESC")
     fun getFavoriteActivities(): Flow<List<ActivityEntity>>
 
